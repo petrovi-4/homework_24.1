0097359521522a22b8926499e37db6f20334200c
petrovi4 <40.10.444@gmail.com>
2024-06-20 13:47:17 +0300

    users done

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('courses.urls', namespace='courses')),
-    # path('user/', include('users.urls', namespace='user'))
+    path('user/', include('users.urls', namespace='user'))
 ]
